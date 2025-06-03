7068e75a93eca9fa14e1341a6ce9e15340c93f80
CamillePastille <camillefasani@gmail.com>
2025-06-03 11:48:52 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,37 +1,3 @@
-/* document.getElementById("button").onclick = async function (e) {
-    e.preventDefault()
-    const email = document.getElementById("email");
-    const passWord = document.getElementById("pass");
-
-    let request = `http://localhost:8000/login`;
-
-    try {
-        let data = await fetch(request);
-        console.log(data)
-        
-        if (!data.ok) {
-            throw new Error(`Erreur HTTP : ${data.status}`);
-        }
-
-        let response = await data.json();
-        console.log(response);
-
-        if (response && response.Is_admin === 0){
-            console.log("Bénévole non administrateur")
-            window.location.replace('volunteer.html');
-
-        }else{
-            console.log("C'est un administrateur")
-            window.location.replace('admin.html');
-        }
-
-    } catch (error) {
-        console.error('Erreur réseau :', error);
-    }
-}; */
-
-
-
 document.getElementById('loginform').addEventListener('submit', async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
